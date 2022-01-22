75de300787ed070265654666a300ddec60719ad1
Dmitry Shulga <dima.shulga.0103@gmail.com>
2022-01-22 13:59:39 +0200

    fix all sorts

---
+++ src/main/java/com/example/sortinglist2/mark_sorter/MarkSorterDesc.java
@@ -0,0 +1,17 @@
+package com.example.sortinglist2.mark_sorter;
+
+import com.example.sortinglist2.Student;
+
+import java.util.Comparator;
+
+public class MarkSorterAsc implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        if (o1 instanceof Student && o2 instanceof Student) {
+            Student s1 = (Student) o1;
+            Student s2 = (Student) o2;
+            return Double.compare(s1.getAvgMark(), s2.getAvgMark());
+        }
+        return 0;
+    }
+}
--- src/main/java/com/example/sortinglist2/name_sorters/NameSorterAsc.java
+++ src/main/java/com/example/sortinglist2/name_sorters/NameSorterAsc.java
@@ -2,13 +2,13 @@ package com.example.sortinglist2;
 
 import java.util.Comparator;
 
-public class NameSorter implements Comparator {
+public class NameSorterAsc implements Comparator {
     @Override
     public int compare(Object o1, Object o2) {
         if (o1 instanceof Student && o2 instanceof Student) {
             Student s1 = (Student) o1;
             Student s2 = (Student) o2;
-            return s1.getName().compareTo(s2.getName());
+            return s2.getName().compareTo(s1.getName());
         }
         return 0;
     }
